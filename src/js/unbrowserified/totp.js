1b89645001b0264efead58d381ccf0363a11a49a
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-10-28 06:22:42 +0900

    add alert for error

--- src/js/unbrowserified/totp.js
+++ src/js/unbrowserified/totp.js
@@ -1,18 +1,20 @@
-const totpGenerator = require('totp-generator');
+const generateToken = require('totp-generator');
 
-selectIfPresent(document.querySelector("#MFALogin > table > tbody > tr:nth-child(1) > td:nth-child(3) > select"), "totp")
+selectIfPresent(document.querySelector("#MFALogin > table > tbody > tr:nth-child(1) > td:nth-child(3) > select"), "totp");
 
 chrome.storage.sync.get(["url"], function(result) {
-    if (result.url !== undefined) {
+    if (result.url !== undefined && result.url !== "") {
         const start = "?secret=";
         const end = "&issuer=";
         let url_after_secret = result.url.substring(result.url.indexOf(start) + start.length);
         const totp = url_after_secret.substring(0, url_after_secret.indexOf(end));
         if (totp !== "") {
-            const token = totpGenerator(totp);
+            const token = generateToken(totp);
             fillIfPresent(document.querySelector("#MFALogin > table > tbody > tr:nth-child(3) > td:nth-child(3) > input[type=text]"), token);
 
-            clickIfPresent(document.querySelector("#MFALogin > input[type=submit]:nth-child(5)"))
+            clickIfPresent(document.querySelector("#MFALogin > input[type=submit]:nth-child(5)"));
         }
+    } else {
+        alert("TOTPのURLが設定されていません");
     }
 });
